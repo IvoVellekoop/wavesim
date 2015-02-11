44b41c78a7d14f7eb0e19f3977ae5e61897586d4
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2015-02-11 04:59:40 +0100

    Fixed bug in scaling of px_range and py_range

--- simgrid.m
+++ simgrid.m
@@ -5,6 +5,8 @@ classdef simgrid
         Nx  % number of grid points in x-dimension
         Ny  % number of grid points in y-dimension
         dx  % grid resolution (x and y)
+        x_margin % amount of grid points on the x_boundary
+        y_margin % amount of grid points on the y_boundary
         x_range % grid point coordinates in x-dimension
         y_range % grid point coordinates in x-dimension
         px_range % grid point coordinates in Fourier transformed x-dimension
@@ -13,15 +15,20 @@ classdef simgrid
     methods
        function obj = simgrid(Nx, Ny, dx)
             % Construct a wave simulation grid object with specified width,
-            % height, and resolution. The wavelength is fixed at 1.
+            % height, and resolution. Nx and Ny must be even.
             %% setup coordinates
-            obj.Nx = Nx;
-            obj.Ny = Ny;
+            obj.Nx = 2^nextpow2(Nx);
+            obj.Ny = 2^nextpow2(Ny);
+            obj.x_margin = (obj.Nx - Nx)/2;
+            obj.y_margin = (obj.Ny - Ny)/2;
+  
             obj.dx = dx;
-            obj.x_range = fftshift(-Nx/2:Nx/2-1)*dx;
-            obj.y_range = fftshift(-Ny/2:Ny/2-1).'*dx;
-            obj.px_range = 2*pi*fftshift(-Nx/2:Nx/2-1)/(dx*Nx);
-            obj.py_range = 2*pi*fftshift(-Ny/2:Ny/2-1).'/(dx*Ny);
+            obj.x_range = fftshift(-obj.Nx/2:obj.Nx/2-1)*dx;
+            obj.y_range = fftshift(-obj.Ny/2:obj.Ny/2-1).'*dx;
+            obj.px_range = 2*pi*fftshift(-obj.Nx/2:obj.Nx/2-1)/(dx*obj.Nx);
+            obj.py_range = 2*pi*fftshift(-obj.Ny/2:obj.Ny/2-1).'/(dx*obj.Ny);
+            
+            disp(['data size set to [',num2str(obj.Nx),',',num2str(obj.Ny),']']);
        end
     end
 end
