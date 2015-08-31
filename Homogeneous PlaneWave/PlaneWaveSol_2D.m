37252ca8d2c5e8137e1593845e9b644ec3cb3521
Saroch <s.leedumrongwatthanakun@student.utwente.nl>
2015-08-31 22:15:38 +0200

    Updating the exact solution for plane wave

--- Homogeneous PlaneWave/PlaneWaveSol_2D.m
+++ Homogeneous PlaneWave/PlaneWaveSol_2D.m
@@ -1,4 +1,4 @@
-function [ sol ] = PlaneWaveSol_2D( k, theta, x, y )
+function [ sol ] = PlaneWaveSol_2D( k, theta,initalphase,h, x, y )
 % ANALYTIC_SOL_2D 
 % Compute the analytic solution to the Helmholtz equation in 2D in
 % homogeneous medim
@@ -9,7 +9,10 @@ function [ sol ] = PlaneWaveSol_2D( k, theta, x, y )
 
     kx = k * cos(theta);
     ky = k * sin(theta);
-    sol = exp(1.0i * (kx * x + ky * y -pi/2 ));%
-    
+    %sol = exp(1.0i * (kx * x + ky * y + pi/2));
+    %sol = ( - exp(1.0i * abs(kx * x + ky * y - initalphase)) + exp(1.0i*abs(kx * x + ky * y))); % normalized
+    %sol = (1/(2*k^2))*( - exp(1.0i * abs(kx * x + ky * y - initalphase)) + exp(1.0i*abs(kx * x + ky * y))); % [o h]
+    %sol = (1/(2*k^2))*( - exp(1.0i * (kx * x + ky * y - initalphase/2)) + exp(1.0i*(kx * x + ky * y + initalphase/2))); % [-h/2 h/2]
+    sol = (-2.0i/(k^3*h))*(exp(1.0i * (kx * x + ky * y - initalphase/2)) + exp(1.0i*(kx * x + ky * y + initalphase/2))- 2*exp(1.0i*(kx * x + ky * y))); % Triangle source [-h/2 h/2]
 end
 
