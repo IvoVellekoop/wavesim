4c326338ef59a8edd76ecdc121b4353ef3bd2aae
Saroch <s.leedumrongwatthanakun@student.utwente.nl>
2015-09-23 11:30:46 +0200

    Updating PSTD

--- PSTD.m
+++ PSTD.m
@@ -18,9 +18,11 @@ classdef PSTD
         callback_interval = 100; %the callback is called every 'callback_interval' steps. Default = 5
         dt; % time step
         it; %iteration
+        number_of_time_steps; % a number of time step
+        cover_time; % time of wave propagation covering roi
         time; % time consumption
         energy_threshold = 1E-9; %the simulation is terminated when the added energy between two iterations is lower than 'energy_threshold'. Default 1E-9
-        max_iterations = 4E3; %or when 'max_iterations' is reached. Default 10000
+        max_iterations = 1E5; %or when 'max_iterations' is reached. Default 10000
         koperator; % k domain laplacian
         dtmax; %maximum time step at sutible pixel_size
     end
@@ -35,6 +37,7 @@ classdef PSTD
             %	sample = SampleMedium object
             %   options.wavelength = free space wavelength (same unit as pixel_size, e. g. um)
             %   options.dt         = time step (leave empty unless forcing a specific value)
+            %   options.time_duration = time duration of whole simulation
             
             fftw('planner','patient'); %optimize fft2 and ifft2 at first use
             options = PSTD.readout_input(options); %fill in default options
@@ -56,21 +59,25 @@ classdef PSTD
             %%  Calculate PSTD laplace operator
             kOper2 = @(px, py) -(px.^2+py.^2); %-k^2
             obj.koperator = (bsxfun(kOper2, sample.grid.px_range, sample.grid.py_range));
-        
+            
+            obj.number_of_time_steps = round(options.time_duration/obj.dt);
             obj.grid = sample.grid;
             obj.roi  = sample.roi;
+            obj.cover_time = options.cover_time; 
             obj.lambda  = options.lambda;
             obj.x_range = sample.grid.x_range(obj.roi{2});
             obj.x_range = obj.x_range - obj.x_range(1);
             obj.y_range = sample.grid.y_range(obj.roi{1});
             obj.y_range = obj.y_range - obj.y_range(1);
+            
         end
         
-        function E = exec(obj, sources)
+        function [ E, en_all, n_time ]= exec(obj, sources)
             tic;
             %% preallocate the loop variables
             E_prev = zeros(obj.grid.N); % field at timestep = n-1
             E      = zeros(obj.grid.N);  % field at timestep = n
+            E_avg = zeros(obj.grid.N); % avg field at steady state
             
             %% initialize_sources_2d (scaling source size to grid size)
             %todo: respect sparsity
@@ -81,6 +88,7 @@ classdef PSTD
             if obj.gpuEnabled                
                 E_prev = gpuArray(E_prev);
                 E = gpuArray(E);
+                E_avg = gpu(E_avg);
                 source = gpuArray(source);
                 obj.coeff = gpuArray(obj.coeff);
                 obj.koperator = gpuArray(obj.koperator);
@@ -95,11 +103,14 @@ classdef PSTD
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             %% PSTD time marching loop
             %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-            obj.it = 1;
+            %obj.it = 1;
             omega  = 2*pi/obj.lambda; %c==1
-            while abs(en_all(obj.it)) >= threshold && obj.it <= obj.max_iterations
-                obj.it = obj.it+1;
-                
+            current_time =0;
+            n_time = 0;
+            for time_step = 1:obj.number_of_time_steps
+            %while abs(en_all(obj.it)) >= threshold && obj.it <= obj.max_iterations
+                obj.it = time_step;
+                current_time = current_time +obj.dt;
                 %calculate source amplitude
                 osc = obj.it * obj.dt * omega; 
                 source_amplitude = exp(-1.0i * osc);
@@ -123,18 +134,41 @@ classdef PSTD
                 %%capture sampled wave fields_2d;
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
                 phase_shift = exp(-1.0i*obj.dt*omega); %expected phase shift for single step
+                % Calculating difference of field
                 en_all(obj.it) = wavesim.energy(E_next-E*phase_shift);
+                % Calculating difference of amplitube of field
+                %en_all(obj.it) = wavesim.energy(abs(E_next)-abs(E*phase_shift));
                 if (mod(obj.it, obj.callback_interval)==0) %now and then, call the callback function to give user feedback
                     obj.callback(obj, E, en_all(1:obj.it), threshold);
                 end
+                
+                if (current_time > obj.cover_time)
+                    if abs(en_all(obj.it)-en_all(obj.it-1)) <= threshold
+                        E_avg = E_avg + (E/source_amplitude);
+                        n_time= n_time+1;
+                    end
+                end
+                
+                %if abs(en_all(obj.it)) <= threshold && current_time > obj.cover_time
+                %    break;
+                %end
+                    
+                %% updating fields
                 E_prev = E;
                 E      = E_next;
+                %obj.it = obj.it+1;
             end %end timestep
-            
-            E = gather(E(obj.roi{1}, obj.roi{2}))/source_amplitude; % converts gpu array back to normal array and normalize phase shift from source
-            obj.time = toc;
-            disp(['Reached steady state in ' num2str(obj.it) ' iterations']);
-            disp(['Time consumption: ' num2str(obj.time) ' s']);
+            E = gather(E_avg(obj.roi{1}, obj.roi{2}))/n_time; 
+            %E = gather(E_prev(obj.roi{1}, obj.roi{2}))/source_amplitude; % converts gpu array back to normal array and normalize phase shift from source
+            %% Simulation finished
+            obj.time=toc;
+            %if abs(en_all(obj.it)) < threshold && current_time > obj.cover_time
+            if (current_time > obj.cover_time) && abs(en_all(obj.it)-en_all(obj.it-1)) <= threshold
+                disp(['Reached steady state in ' num2str(obj.it) ' iterations']);
+                disp(['Time consumption: ' num2str(obj.time) ' s']);
+            else
+                disp('Did not reach steady state');
+            end
         end
         
     end
