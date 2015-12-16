8c190a1e8592e62f057e34021a90b580b9658608
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2015-12-16 17:04:52 +0100

    Updated code to make accuracy plot

--- Tests/figure_accuracy_plot.m
+++ Tests/figure_accuracy_plot.m
@@ -1,13 +1,12 @@
 addpath('..');
-last = 21;
 load('figure_accuracy.mat');
-theory = 1./iterations_per_wavelength.^4 * relative_error(2) * iterations_per_wavelength(2).^4;
-loglog(iterations_per_wavelength(2:last), theory(2:last), 'k', 'LineWidth', 2.0);
+theory = 1./iterations_per_wavelength.^4 * relative_error(6) * iterations_per_wavelength(6).^4;
+loglog(iterations_per_wavelength(2:end), theory(2:end), 'k', 'LineWidth', 2.0);
 hold on
-loglog([0.1, 3E4], relative_error(1)*[1,1], ':');
+loglog([0.1, 10E4], relative_error(1)*[1,1], ':');
 loglog(iterations_per_wavelength(1), relative_error(1), 's', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'k', 'MarkerSize', 8);
-loglog(iterations_per_wavelength(2:last), relative_error(2:last), 'r+', 'MarkerSize', 10);
-xlim([0.1, 3E4]);
+loglog(iterations_per_wavelength(2:end), relative_error(2:end), 'r+', 'MarkerSize', 10);
+xlim([0.1, 5E4]);
 hold off;
 fixplot('Iterations per wavelength', 'Relative error', [8 7], '');
 fp = '../../wavesimpaper/figures/';
