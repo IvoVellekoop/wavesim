9ffe6e13f200700660edf6f464278a0f03eef664
Gerwin <gosnabrugge@gmail.com>
2016-01-03 20:41:01 +0100

    Repeated random medium simulations with medium with n > 1 everywhere. Also the imaginary part of the refractive index is now also normally distributed.

--- Tests/random_medium.m
+++ Tests/random_medium.m
@@ -1,10 +1,7 @@
+%%% Simulates the wave propagation of a point source in a random medium
+%%% Demonstrate that PSTD converges to wavesim solution as dt decreases
 
-% Simulates the propagation of a plane wave through a homogeneous medium
-% with n=1
-% Analyzes the accuracy of wavesim and PSTD (with varying time steps)
-
-%close all;
-% clear all; close all;
+clear all; close all;
 addpath('..');
 
 %% options for grid (gopt) and for simulation (sopt) 
@@ -19,47 +16,49 @@ dt_relative_range = [0,1./2.^(0:0.5:10.5)];
 mopt.lambda = sopt.lambda;
 mopt.pixel_size = sopt.lambda/PPW;
 mopt.boundary_widths = [0, 0]; %periodic boundaries
-mopt.boundary_strength = 0.2;
+mopt.boundary_strength = 0;
 mopt.boundary_type = 'PML3';
 N = [64*PPW 64*PPW]; % size of medium (in pixels)
+
+%% Preallocate PSTD data
+E_PSTD = cell(1,length(dt_relative_range)-1);
+errors_PSTD = zeros(1,length(E_PSTD));
+iterations_per_wavelength = zeros(1, length(dt_relative_range));
+
+%% Construct random medium
+% real refractive index
+n0 = 1.3;          % mean
+n_var = 0.1;     % variance
+
+% imaginary refractive index
+a0 = 0.05;        % mean
+a_var = 0.02;    % variance
+
+% randomly generate complex refractive index map
+n_sample = 1.0*(n0 + n_var * randn(N)) + 1.0i*(a0 + a_var * randn(N));
+
+% low pass filter to remove sharp edges
+n_fft = fft2(n_sample);
+window = fftshift(hamming(N(1)) * hamming(N(2))');
+n_sample = ifft2(n_fft.*window);
+
+% construct sample object
+sample = SampleMedium(n_sample, mopt); 
 
 %% define a point source at the medium center
 source = sparse(N(1), N(2));
 source(end/2,end/2) = 1; % point source
 
-%% simulate wave propagation for a variety of refractive indices
-%reserve space for output data
-errors_PSTD = zeros(1,length(E_PSTD));
-iterations_per_wavelength = zeros(1, length(dt_relative_range));
-E_PSTD = cell(1,length(dt_relative_range)-1);
-
-%% Construct random medium
-size = PPW; %size MAF window 
-
-n0 = 1;         %background medium reffractive index
-ndiff = 0.2;    %refractive index variance
-k = 0.1;       %imaginary part
-n_sample = ndiff*randn(N) + n0 + 1.0i*k*rand(N);
-
-% low pass filter
-n_fft = fft2(n_sample);
-window = fftshift(hamming(N(1)) * hamming(N(2))');
-n_sample = ifft2(n_fft.*window);
-
-%% construct medium with desired refractive index and boundary conditions
-sample = SampleMedium(n_sample, mopt);
-disp(['Estimated leakage' num2str(sample.leakage^4)]);
-
 %% wavesim simulation
 sim = wavesim(sample, sopt);
 iterations_per_wavelength(1) = sim.iterations_per_cycle;
-[E_wavesim, state] = exec(sim, source);
+E_wavesim = exec(sim, source);
 
-%% PST simulations with varying time step size
+%% PSTD simulations with varying time step size
 for t_i=2:length(dt_relative_range)
     sopt.dt_relative = dt_relative_range(t_i);
     sim_PSTD = PSTD(sample, sopt);
     iterations_per_wavelength(t_i) = sim_PSTD.iterations_per_cycle;
-    [E_PSTD{t_i-1}, state] = exec(sim_PSTD, source);
-    errors_PSTD(n) = mean2(abs(E_PSTD{t_i-1} - E_wavesim)) / mean2(abs(E_wavesim).^2);
+    E_PSTD{t_i-1} = exec(sim_PSTD, source);
+    errors_PSTD(t_i-1) = mean2(abs(E_PSTD{t_i-1} - E_wavesim)) / mean2(abs(E_wavesim).^2);
 end
--- Tests/random_medium_plot.m
+++ Tests/random_medium_plot.m
@@ -1,28 +1,29 @@
-load('random_medium_results.mat')
+load('random_medium_results2.mat')
 close all;
 %% plot results
 fig_size = [8 7];
 loglog(iterations_per_wavelength(2:end),errors_PSTD,'r+', 'MarkerSize', 10);
 fixplot('Iterations per wavelength', 'Relative error', fig_size, '');
 
-%% refractive index map image
+%% refractive index map image (real & imaginary)
 x = 1:N(2)/PPW;
 y = 1:N(1)/PPW;
 
 figure(2);
-imagesc(x,y,real(sim.V))
+imagesc(x,y,real(n_sample))
 h = colorbar;
 set(get(h,'Title'),'String','Re\{V\}','FontSize',16);
 fixplot('x (\lambda)','y (\lambda)',fig_size,'');
 axis square;
 
 figure(3);
-imagesc(x,y,imag(sim.V))
+imagesc(x,y,imag(n_sample))
 h = colorbar;
 set(get(h,'Title'),'String','Im\{V\}','FontSize',16);
 fixplot('x (\lambda)','y (\lambda)',fig_size,'');
 axis square;
 
+%% Field solutions (wavesim & PSTD)
 figure(4);
 imagesc(x,y,log(abs(E_wavesim)))
 h = colorbar;
