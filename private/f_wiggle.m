9038472033260d6619dc4586b07dcf4eab5c2501
IvoVellekoop <IvoVellekoop@users.noreply.github.com>
2018-04-23 10:22:32 +0200

    Minor changes

--- private/f_wiggle.m
+++ private/f_wiggle.m
@@ -1,5 +1,7 @@
 function E = f_wiggle(E, gx, gy, gz)
 %f_wiggle Multiply E by the three gradient vectors gx, gy, gz
+%todo: somehow this step is really slow even on the gpu!
+%todo: ideally this could be implemented in the fft itself
     E = E .* gx .* gy .* gz;
 end
 
