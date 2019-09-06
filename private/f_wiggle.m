c4978ceceeeed7e678ff1ee91246d441dc06d29e
Daan de Ruiter <daanjderuiter@hotmail.nl>
2019-09-06 14:18:04 +0200

    Added indentation for stylistic consistency
    Function files were not all indented

--- private/f_wiggle.m
+++ private/f_wiggle.m
@@ -1,7 +1,7 @@
 function E = f_wiggle(E, g1, g2, g3)
-% f_wiggle Multiply E by the three gradient vectors g1, g2, g3. method is
-% used by both x-space and k-space wiggle algorithm
-%todo: somehow this step is really slow even on the gpu!
+    % f_wiggle Multiply E by the three gradient vectors g1, g2, g3. method is
+    % used by both x-space and k-space wiggle algorithm
+    %todo: somehow this step is really slow even on the gpu!
     E = E .* g1 .* g2 .* g3;
 end
 
